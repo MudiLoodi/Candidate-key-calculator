327bf75d706159b9c5763ba93f6e32a053c7896b
MudiLoodi <wadi.38@hotmail.com>
2022-06-25 03:44:12 +0200

    Finds attribute closure

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 
+import functools
+
+
 def get_input():
     # R: A, B, C, D, E
     relation_input = input("Enter the relation: ")
@@ -25,15 +28,42 @@ def categorize_attributes(fd_lst, attr_lst):
 
     attributes_left = [fd for fd in attributes_temp_left if fd not in attributes_temp_right]
     attributes_right = [fd for fd in attributes_temp_right if fd not in attributes_temp_left]
+    attributes_both_left_right = [fd for fd in attributes_temp_left if fd in attributes_temp_right]
 
     attributes_not_left_right = []
     for attribute in attr_lst:
         if attribute not in "".join(fd_lst):
             attributes_not_left_right.append(attribute)
-    return attributes_left, attributes_right, attributes_not_left_right
+    return attributes_left, attributes_right, attributes_not_left_right, attributes_both_left_right
 
 
-print(categorize_attributes(functional_depend_lst, attribute_lst))
+attributes_left, attributes_right, attributes_not_left_right, attributes_both_left_right = categorize_attributes(functional_depend_lst, attribute_lst)
+
+closure = {}
+
+#if  len(attributes_not_left_right) == 0 and  len(attributes_left) == 0: # Check if lists are empty
+combination = attributes_not_left_right + attributes_left
+
+def get_closure(attributes):
+    for i in range(0, len(functional_depend_lst)):
+        for attribute in attributes: # ["E", "D"]
+            if attribute in functional_depend_lst[i]:
+                closure[attribute] = functional_depend_lst[i].replace("->", " ")
+            elif attribute in closure: # Fixes bug where an attribute will be update because it is checked twice for different FD
+                pass
+            else:
+                closure[attribute] = attribute
+    closure_lst = list(functools.reduce(lambda x, y: x + y, closure.items()))
+    print(closure)
+    closure_str = "".join(closure_lst).replace(" ", "")
+    closure_res = "".join(set(closure_str))
+    return closure_res
+
+if len(get_closure(combination)) == len(attribute_lst):
+    print("Final ", get_closure(combination) )
+else:
+    perm = ["".join((x,y)) for x in attributes_both_left_right for y in combination] 
+    print(get_closure(perm))
 
 
 
